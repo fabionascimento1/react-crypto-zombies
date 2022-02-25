b87772beeae601dac251448b129d2c39e5f62a6c
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-25 10:10:16 -0300

    feat: ensure dinamic Routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
+import {lazy, Suspense} from 'react'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+
+const Login = lazy(() => import('./pages/Login'))
+const Main = lazy(() => import('./pages/Main'))
 
 function App() {
-
   return (
-   <>app</>
+   <BrowserRouter>
+    <ul>
+      <li><Link to="/">Main</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+    <Suspense fallback='Carregando ...'>
+      <Routes>
+        <Route path="/*" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+      </Suspense>
+   </BrowserRouter>
   )
 }
 
